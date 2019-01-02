ebd16bdcfc9d5980d0be1ee7e3f7bab754bcafc3
nwatabou <wataru1.11@gmail.com>
2019-01-02 23:22:46 +0900

    プロジェクトファイル追加

---
+++ TestXcodeServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestXcodeServer
+//
+//  Created by nancy on 2019/01/02.
+//  Copyright © 2019年 WataruNakanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
